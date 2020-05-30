1c4f5fecc192741dc091b2e10f79ebf189b7db3b
Falansh Siddh <56497318+fsiddh@users.noreply.github.com>
2020-05-30 17:21:27 +0530

    Add files via upload

---
+++ Projects/Weather Chatbots/2. Weather Chatbot (RASA NLU)/actions.py
@@ -0,0 +1,29 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from weather_report import weather
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_weather_api"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        city = tracker.latest_message['text']
+        temp = int(weather(city)['temp']-273)
+        dispatcher.utter_template("utter_temp", tracker, temp=temp)
+
+        return []
---
+++ Projects/Weather Chatbots/2. Weather Chatbot (RASA NLU)/data/stories.md
@@ -0,0 +1,13 @@
+## happy weather
+* greet
+  - utter_greet
+* weather
+  - utter_city
+* city
+  - action_weather_api
+  
+
+## say goodbye
+* goodbye
+  - utter_goodbye
+
