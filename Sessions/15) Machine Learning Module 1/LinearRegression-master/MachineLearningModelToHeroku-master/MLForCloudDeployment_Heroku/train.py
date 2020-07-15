31620e3ce7d89dd6c915d19cb50cd53c4ba791fe
Falansh Siddh <jasonshaw546@gmail.com>
2020-07-15 19:31:39 +0530

    Seesion file

---
+++ Sessions/15) Machine Learning Module 1/LinearRegression-master/MachineLearningModelToHeroku-master/MLForCloudDeployment_Heroku/train.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import pickle
+from sklearn.metrics import  r2_score
+print('t')
